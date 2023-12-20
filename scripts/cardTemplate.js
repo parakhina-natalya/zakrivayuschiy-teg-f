f3448c1eec66d4a52139e9123bed65c4750c48d8
parakhina-natalya <valmond.scholz@yandex.ru>
2023-12-21 01:02:59 +0300

    сверстать лендинг

---
+++ scripts/cardTemplate.js
@@ -0,0 +1,15 @@
+import { cardsContent } from '../scripts/cardsContent.js';
+
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsContainer = document.querySelector('.cards-container');
+
+cardsContent.forEach((card) => {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__heading').textContent = card.heading;
+  cardElement.querySelector('.card__image').src = card.image;
+  cardElement.querySelector('.card__image').alt = card.heading;
+  cardElement.querySelector('.paragraph-one').textContent = card.paragraphOne;
+  cardElement.querySelector('.paragraph-two').textContent = card.paragraphTwo;
+
+  cardsContainer.append(cardElement);
+});
